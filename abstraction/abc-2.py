8fee9e994b24d3fbab731e9de2e345f89d1e9fca
philips <philip8275@gmail>
2025-03-22 21:03:30 +0530

    Second abstraction file

--- abstraction/abc-2.py
+++ abstraction/abc-2.py
@@ -10,3 +10,19 @@ class Animal(ABC):
     @abstractmethod
     def move(self):
         pass
+class Dog(Animal):
+    def sound(self):
+        return "Bhav bhav"
+    def move(self):
+        return "Run"
+class Bird(Animal):
+    def sound(self):
+        return "chiv chiv"
+    def move(self):
+        return "Fly high in the sky"
+
+obj_dog = Dog()
+obj_bird = Bird()
+
+print(f"Dog sounds --> {obj_dog.sound()} and move --> {obj_dog.move()}")
+print(f"Dog sounds --> {obj_bird.sound()} and move --> {obj_bird.move()}")
